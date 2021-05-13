972a33074daa70b3c077f96fc17cb66b21d631ef
Printice Lawton <printicelaw10@gmail.com>
2021-05-13 16:05:39 -0700

    restored markdown.js

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,26 @@
+
+// TODO: Create a function to generate markdown for README
+
+function generateMarkdown(data) {
+  return `
+  
+# Project Name
+${data.projectname}
+
+# Description
+${data.description}
+
+# Motivation 
+${data.motivation}
+
+# Contact Information 
+
+* GitHub Username: ${data.username}
+* Contact Email: ${data.userEmail}
+`;
+}
+
+//need to export the generateMarkdown function 
+module.exports = generateMarkdown;
+
+module.exports = generateMarkdown;
